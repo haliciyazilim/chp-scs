48cac334145ad854aae4fcfccdfc4c4f6762291d
Alperen Kavun <alperen@halici.com.tr>
2013-06-13 15:10:32 +0300

    Added colors and fonts define macros, will be continued

--- ios/CHP-SCS/CHP-SCS/TypeDefs.h
+++ ios/CHP-SCS/CHP-SCS/TypeDefs.h
@@ -9,6 +9,20 @@
 #ifndef CHP_SCS_TypeDefs_h
 #define CHP_SCS_TypeDefs_h
 
-#define DEFAULT_FONT_NAME @""
+#define LIGHT_FONT_NAME @"SWZ721L"
+#define MEDIUM_FONT_NAME @"SWZ721M"
+#define THIN_FONT_NAME @"SWZ721T"
+
+
+#define TOP_BAR_BACKGROUND_COLOR [UIColor colorWithRed:166.0/255.0 green:56.0/255.0 blue:46.0/255.0 alpha:1.0]
+#define TOP_BAR_TEXT_COLOR [UIColor colorWithRed:223.0/255.0 green:200.0/255.0 blue:187.0/255.0 alpha:1.0]
+#define TOP_BAR_TEXT_FONT [UIFont fontWithName:@"SWZ721M" size:22.0]
+
+#define MENU_BACKGROUND_COLOR [UIColor colorWithRed:50.0/255.0 green:46.0/255.0 blue:37.0/255.0 alpha:1.0]
+#define MENU_SEPERATOR_COLOR [UIColor colorWithRed:35.0/255.0 green:32.0/255.0 blue:26.0/255.0 alpha:1.0]
+
+#define MAIN_CONTENT_BACKGROUND_COLOR [UIColor colorWithRed:80.0/255.0 green:76.0/255.0 blue:64.0/255.0 alpha:1.0]
+#define MAIN_CONTENT_SEPERATOR_COLOR [UIColor colorWithRed:50.0/255.0 green:46.0/255.0 blue:37.0/255.0 alpha:1.0]
+
 
 #endif

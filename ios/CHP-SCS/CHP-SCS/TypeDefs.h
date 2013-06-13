6b10e70fd3b94d8354a5c21c4209dcc8803960ab
Alperen Kavun <alperen@halici.com.tr>
2013-06-13 17:43:35 +0300

    added fonts and colors related macros

--- ios/CHP-SCS/CHP-SCS/TypeDefs.h
+++ ios/CHP-SCS/CHP-SCS/TypeDefs.h
@@ -9,20 +9,43 @@
 #ifndef CHP_SCS_TypeDefs_h
 #define CHP_SCS_TypeDefs_h
 
+// font names macros
 #define LIGHT_FONT_NAME @"SWZ721L"
 #define MEDIUM_FONT_NAME @"SWZ721M"
 #define THIN_FONT_NAME @"SWZ721T"
 
-
+// top bar related macros
 #define TOP_BAR_BACKGROUND_COLOR [UIColor colorWithRed:166.0/255.0 green:56.0/255.0 blue:46.0/255.0 alpha:1.0]
 #define TOP_BAR_TEXT_COLOR [UIColor colorWithRed:223.0/255.0 green:200.0/255.0 blue:187.0/255.0 alpha:1.0]
 #define TOP_BAR_TEXT_FONT [UIFont fontWithName:@"SWZ721M" size:22.0]
 
+// menu related macros
 #define MENU_BACKGROUND_COLOR [UIColor colorWithRed:50.0/255.0 green:46.0/255.0 blue:37.0/255.0 alpha:1.0]
 #define MENU_SEPERATOR_COLOR [UIColor colorWithRed:35.0/255.0 green:32.0/255.0 blue:26.0/255.0 alpha:1.0]
 
+// main content related macros
 #define MAIN_CONTENT_BACKGROUND_COLOR [UIColor colorWithRed:80.0/255.0 green:76.0/255.0 blue:64.0/255.0 alpha:1.0]
 #define MAIN_CONTENT_SEPERATOR_COLOR [UIColor colorWithRed:50.0/255.0 green:46.0/255.0 blue:37.0/255.0 alpha:1.0]
+#define MAIN_CONTENT_SUBHEADER_TEXT_COLOR [UIColor colorWithRed:244.0/255.0 green:61.0/255.0 blue:44.0/255.0 alpha:1.0]
+#define MAIN_CONTENT_SUBHEADER_TEXT_FONT [UIFont fontWithName:@"SWZ721L" size:10.0]
+
+// card related macros
+#define CARD_YEAR_TEXT_COLOR [UIColor colorWithRed:80.0/255.0 green:76.0/255.0 blue:64.0/255.0 alpha:1.0]
+#define CARD_YEAR_TEXT_FONT [UIFont fontWithName:@"SWZ721L" size:11.0]
+#define CARD_HEADER_TEXT_COLOR [UIColor colorWithRed:217.0/255.0 green:74.0/255.0 blue:61.0/255.0 alpha:1.0]
+#define CARD_HEADER_TEXT_FONT [UIFont fontWithName:@"SWZ721L" size:14.0]
+#define CARD_NAME_TEXT_COLOR [UIColor colorWithRed:50.0/255.0 green:46.0/255.0 blue:37.0/255.0 alpha:1.0]
+#define CARD_NAME_TEXT_FONT [UIFont fontWithName:@"SWZ721M" size:22.0]
+#define CARD_CHEST_NO_TEXT_COLOR [UIColor colorWithWhite:1.0 alpha:1.0]
+#define CARD_CHEST_NO_TEXT_FONT [UIFont fontWithName:@"SWZ721T" size:50.0]
+#define CARD_PROVINCE_TEXT_COLOR [UIColor colorWithWhite:1.0 alpha:1.0]
+#define CARD_PROVINCE_TEXT_FONT [UIFont fontWithName:@"SWZ721M" size:17.0]
+#define CARD_DISTRICT_TEXT_COLOR [UIColor colorWithWhite:1.0 alpha:1.0]
+#define CARD_DISTRICT_TEXT_FONT [UIFont fontWithName:@"SWZ721L" size:17.0]
+#define CARD_OTHER_CONTENT_TEXT_COLOR [UIColor colorWithWhite:1.0 alpha:1.0]
+#define CARD_OTHER_CONTENT_TEXT_FONT [UIFont fontWithName:@"SWZ721M" size:17.0]
+#define CARD_SECOND_OFFICER_TEXT_COLOR [UIColor colorWithWhite:1.0 alpha:1.0]
+#define CARD_SECOND_OFFICER_TEXT_FONT [UIFont fontWithName:@"SWZ721L" size:10.0]
 
 
 #endif

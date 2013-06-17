8084068fa0b3c077bee5681c616ae0455be51100
Alperen Kavun <alperen@halici.com.tr>
2013-06-17 14:33:23 +0300

    font macros are fixed

--- ios/CHP-SCS/CHP-SCS/TypeDefs.h
+++ ios/CHP-SCS/CHP-SCS/TypeDefs.h
@@ -10,44 +10,53 @@
 #define CHP_SCS_TypeDefs_h
 
 // font names macros
-#define LIGHT_FONT_NAME @"SWZ721L"
-#define MEDIUM_FONT_NAME @"SWZ721M"
-#define THIN_FONT_NAME @"SWZ721T"
+#define LIGHT_FONT_NAME @"Swis 721 Lt BT"
+#define MEDIUM_FONT_NAME @"Swis721 Md BT"
+#define THIN_FONT_NAME @"Swis721 Th BT"
 
 // top bar related macros
 #define TOP_BAR_BACKGROUND_COLOR [UIColor colorWithRed:166.0/255.0 green:56.0/255.0 blue:46.0/255.0 alpha:1.0]
 #define TOP_BAR_TEXT_COLOR [UIColor colorWithRed:223.0/255.0 green:200.0/255.0 blue:187.0/255.0 alpha:1.0]
-#define TOP_BAR_TEXT_FONT [UIFont fontWithName:@"SWZ721M" size:22.0]
+#define TOP_BAR_TEXT_FONT [UIFont fontWithName:@"Swis721 Md BT" size:18.0]
 
 // menu related macros
 #define MENU_BACKGROUND_COLOR [UIColor colorWithRed:50.0/255.0 green:46.0/255.0 blue:37.0/255.0 alpha:1.0]
 #define MENU_SEPERATOR_COLOR [UIColor colorWithRed:35.0/255.0 green:32.0/255.0 blue:26.0/255.0 alpha:1.0]
 #define MENU_TEXT_COLOR [UIColor colorWithRed:130.0/255.0 green:123.0/255.0 blue:118.0/255.0 alpha:1.0]
-#define MENU_TEXT_FONT [UIFont fontWithName:@"SWZ721L" size:22.0] 
+#define MENU_TEXT_FONT [UIFont fontWithName:@"Swis721 Md BT" size:18.0] 
 
 // main content related macros
 #define MAIN_CONTENT_BACKGROUND_COLOR [UIColor colorWithRed:80.0/255.0 green:76.0/255.0 blue:64.0/255.0 alpha:1.0]
 #define MAIN_CONTENT_SEPERATOR_COLOR [UIColor colorWithRed:50.0/255.0 green:46.0/255.0 blue:37.0/255.0 alpha:1.0]
 #define MAIN_CONTENT_SUBHEADER_TEXT_COLOR [UIColor colorWithRed:244.0/255.0 green:61.0/255.0 blue:44.0/255.0 alpha:1.0]
-#define MAIN_CONTENT_SUBHEADER_TEXT_FONT [UIFont fontWithName:@"SWZ721L" size:10.0]
+#define MAIN_CONTENT_SUBHEADER_TEXT_FONT [UIFont fontWithName:@"Swis721 Lt BT" size:12.0]
 
 // card related macros
 #define CARD_YEAR_TEXT_COLOR [UIColor colorWithRed:80.0/255.0 green:76.0/255.0 blue:64.0/255.0 alpha:1.0]
-#define CARD_YEAR_TEXT_FONT [UIFont fontWithName:@"SWZ721L" size:11.0]
+#define CARD_YEAR_TEXT_FONT [UIFont fontWithName:@"Swis721 Lt BT" size:12.0]
+
 #define CARD_HEADER_TEXT_COLOR [UIColor colorWithRed:217.0/255.0 green:74.0/255.0 blue:61.0/255.0 alpha:1.0]
-#define CARD_HEADER_TEXT_FONT [UIFont fontWithName:@"SWZ721L" size:14.0]
+#define CARD_HEADER_TEXT_FONT [UIFont fontWithName:@"Swis721 Lt BT" size:14.0]
+
 #define CARD_NAME_TEXT_COLOR [UIColor colorWithRed:50.0/255.0 green:46.0/255.0 blue:37.0/255.0 alpha:1.0]
-#define CARD_NAME_TEXT_FONT [UIFont fontWithName:@"SWZ721M" size:22.0]
+#define CARD_NAME_TEXT_FONT [UIFont fontWithName:@"Swis721 Md BT" size:20.0]
+
 #define CARD_CHEST_NO_TEXT_COLOR [UIColor colorWithWhite:1.0 alpha:1.0]
-#define CARD_CHEST_NO_TEXT_FONT [UIFont fontWithName:@"SWZ721T" size:50.0]
+#define CARD_CHEST_NO_TEXT_FONT [UIFont fontWithName:@"Swis721 Th BT" size:50.0]
+
 #define CARD_PROVINCE_TEXT_COLOR [UIColor colorWithWhite:1.0 alpha:1.0]
-#define CARD_PROVINCE_TEXT_FONT [UIFont fontWithName:@"SWZ721M" size:17.0]
+#define CARD_PROVINCE_TEXT_FONT [UIFont fontWithName:@"Swis721 Md BT" size:17.0]
+
 #define CARD_DISTRICT_TEXT_COLOR [UIColor colorWithWhite:1.0 alpha:1.0]
-#define CARD_DISTRICT_TEXT_FONT [UIFont fontWithName:@"SWZ721L" size:17.0]
+#define CARD_DISTRICT_TEXT_FONT [UIFont fontWithName:@"Swis721 Lt BT" size:17.0]
+
 #define CARD_OTHER_CONTENT_TEXT_COLOR [UIColor colorWithWhite:1.0 alpha:1.0]
-#define CARD_OTHER_CONTENT_TEXT_FONT [UIFont fontWithName:@"SWZ721M" size:17.0]
+#define CARD_OTHER_CONTENT_TEXT_FONT [UIFont fontWithName:@"Swis721 Md BT" size:17.0]
+
 #define CARD_SECOND_OFFICER_TEXT_COLOR [UIColor colorWithWhite:1.0 alpha:1.0]
-#define CARD_SECOND_OFFICER_TEXT_FONT [UIFont fontWithName:@"SWZ721L" size:10.0]
+#define CARD_SECOND_OFFICER_TEXT_FONT [UIFont fontWithName:@"Swis721 Lt BT" size:10.0]
+
+#define CARD_OFFICER_IMAGE_BORDER_COLOR [UIColor colorWithRed:233.0/255.0 green:217.0/255.0 blue:208.0/255.0 alpha:1.0]
 
 
 #endif

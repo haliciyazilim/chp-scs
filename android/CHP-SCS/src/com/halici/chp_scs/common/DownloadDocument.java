8d227da9096b323da320a57c5a2fade52fa07193
Abdullah Karacabey <abdullah@edebimce.com>
2013-07-04 17:57:50 +0300

    Android - update

---
+++ android/CHP-SCS/src/com/halici/chp_scs/common/DownloadDocument.java
@@ -0,0 +1,91 @@
+package com.halici.chp_scs.common;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Date;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.os.Environment;
+
+public class DownloadDocument {
+
+	private File root;
+	private String documentName;
+	private String url;
+	
+	public DownloadDocument(String url) {
+		this.url=url;
+		this.root=new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/CHP-SCS");
+		
+		String[] urlArray=this.url.split("/");
+		this.documentName=urlArray[urlArray.length-1];
+		
+		if(this.root.exists()==false)
+			root.mkdir();
+	}
+	
+
+
+	public static long getLastModifiedDate(String url){
+		
+		HttpURLConnection.setFollowRedirects(false);
+		HttpURLConnection con;
+		long date = 0;
+		try {
+			con = (HttpURLConnection) new URL(url).openConnection();
+			date = con.getLastModified();
+
+			if (date == 0)
+				System.out.println("No last-modified information.");
+			else
+				System.out.println("Last-Modified: " + new Date(date));
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		return date;
+	}
+	
+	
+	public String downloadDocument(){
+		File documentOnLocal=new File(this.root,this.documentName);
+		
+		if(documentOnLocal.exists()){
+			long lastMofiedDateOnServer=getLastModifiedDate(this.url);
+			
+			if(lastMofiedDateOnServer<documentOnLocal.lastModified()){
+				System.out.println("Document is allready exist! Document is not changed.");
+				return this.root+"/"+this.documentName;
+			}
+		}
+		try {
+			System.out.println("Document is downloading!");
+			new DefaultHttpClient().execute(new HttpGet(url))
+			.getEntity().writeTo(
+			        new FileOutputStream(new File(this.root,this.documentName)));
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return this.root+"/"+this.documentName;
+	}
+	
+}
